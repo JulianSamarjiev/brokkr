06fb2c70270762247081bd842d62685a9a9a6ca5
JulianSamarjiev <juliansamarjiev@gmail.com>
2016-07-14 18:46:55 +0200

    Add brokkr email as contact form recipient

--- mailer.php
+++ mailer.php
@@ -18,7 +18,7 @@
 
     // Set the recipient email address.
     // FIXME: Update this to your desired email address.
-    $recipient = "marin.gerov@gmail.com";
+    $recipient = "contact@brokkr.co";
 
     // Set the email subject.
     $subject = "New contact from $name";
@@ -35,7 +35,7 @@
     if (mail($recipient, $subject, $email_content, $email_headers)) {
       // Set a 200 (okay) response code.
       http_response_code(200);
-      echo "Thank You! Your message has been sent.";
+      echo "Thank You! Talk to you soon!.";
     } else {
       // Set a 500 (internal server error) response code.
       http_response_code(500);
